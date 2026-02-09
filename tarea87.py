5a13b6d0e647b3670f35ee534d1f91641c327598
Josearmando34 <josearmandojimenezvicente979@gmail.com>
2026-02-09 07:36:39 -0600

    Add files via upload

---
+++ tarea87.py
@@ -0,0 +1,8 @@
+TAM = 5  # tamaño del arreglo (cámbialo aquí y se actualiza todo)
+
+calificaciones = [0] * TAM
+
+for i in range(TAM):
+    calificaciones[i] = int(input("Captura la calificación: "))
+
+print(calificaciones)
